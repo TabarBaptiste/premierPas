617eb9951c2654c4d48a1b033e2ca1764c77eff6
Baptiste <tabarbaptiste@gmail.com>
2023-03-04 19:46:52 +0100

    Message de commit

--- Game.ts
+++ Game.ts
@@ -1,59 +1,128 @@
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 // Définition d'un type union pour les forces possibles
 type Force =
     typeof Personnage.FORCE_ZERO
     | typeof Personnage.FORCE_PETITE
     | typeof Personnage.FORCE_MOYENNE
-    | typeof Personnage.FORCE_GRANDE;
+    | typeof Personnage.FORCE_GRANDE
+    | number;
 
-// Définition de la classe Personnage
-class Personnage {
-
-    // Définition de constantes statiques pour les forces possibles
-    static readonly FORCE_PETITE = 20;
-    static readonly FORCE_MOYENNE = 50;
-    static readonly FORCE_GRANDE = 80;
-    static readonly FORCE_ZERO = 0;
-
-    // Définition des attributs privés de la classe
-    private _force: Force;
-    private _nom: string;
-
-    // Définition du constructeur de la classe Personnage, prenant une force initiale et un nom optionnels
-    public constructor(forceInitiale: Force = Personnage.FORCE_ZERO, nom: string = "") {
-        this._force = forceInitiale; // Initialisation de l'attribut _force avec la valeur passée en paramètre ou la valeur par défaut FORCE_ZERO
-        this._nom = nom; // Initialisation de l'attribut _nom avec la valeur passée en paramètre ou une chaîne vide
-    }
-
-    // Définition d'un getter pour l'attribut _force
-    public getForce(): Force {
-        return this._force;
-    }
-
-    // Définition d'un setter pour l'attribut _force
-    public setForce(force: Force): void {
-        if (this._force !== force) { // Si la nouvelle force est différente de l'ancienne
-            this._force = force; // On met à jour l'attribut _force avec la nouvelle force
+    class Personnage {
+        // Définition de constantes statiques pour les forces possibles
+        static readonly FORCE_PETITE = 20;
+        static readonly FORCE_MOYENNE = 50;
+        static readonly FORCE_GRANDE = 80;
+        static readonly FORCE_ZERO = 0;
+    
+        // Déclaration des attributs de la classe
+        private _force: number; // La force du personnage
+        private _nom: string | null; // Le nom du personnage, peut être null
+    
+        // Constructeur de la classe, prenant une force initiale et un nom optionnels
+        public constructor(forceInitiale: Force = Personnage.FORCE_ZERO, nom: string = "") {
+            // Initialisation de l'attribut _force avec la valeur passée en paramètre ou la valeur par défaut FORCE_ZERO
+            this._force = forceInitiale;
+            // Initialisation de l'attribut _nom avec la valeur passée en paramètre ou null
+            this._nom = nom !== null && nom !== "" ? nom : null;
+        }
+    
+        // Définition d'un getter pour l'attribut _force
+        public getForce(): number {
+            return this._force;
+        }
+    
+        // Définition d'un setter pour l'attribut _force
+        public setForce(force: number): void {
+            this._force = force;
+        }
+    
+        // Définition d'un getter pour l'attribut _nom
+        public getNom(): string {
+            return this._nom;
+        }
+    
+        // Définition d'un setter pour l'attribut _nom, vérifiant que le nom n'est pas null ou une chaîne vide
+        public setNom(nom: string | null): void {
+            if (nom !== null && nom !== "") {
+                this._nom = nom;
+            }
+        }
+    
+        // Méthode permettant à un personnage d'entrer en contact avec une chose
+        public entrerEnContact(chose: Chose): void {
+            this.modifierForce(chose);
+        }
+    
+        // Méthode privée qui modifie la force du personnage en fonction de la chose en contact
+        private modifierForce(chose: Chose): void {
+            if (chose instanceof ChoseBienveillante) {
+                this._force += chose.getPotentielDeContact();
+            } else if (chose instanceof ChoseHostile) {
+                this._force -= chose.getPotentielDeContact();
+            }
         }
     }
+    
 
-    // Définition d'un getter pour l'attribut _nom
-    public getNom(): string {
-        return this._nom;
+// Définition de la classe Chose
+class Chose {
+    // Définition de la propriété potentielDeContact
+    private _potentielDeContact: number;
+
+    // Constructeur de la classe Chose
+    constructor(potentielDeContact: number) {
+        // Initialisation de la propriété potentielDeContact
+        this._potentielDeContact = potentielDeContact;
     }
 
-    // Définition d'un setter pour l'attribut _nom
-    public setNom(nom: string): void {
-        if (this._nom !== nom) { // Si le nouveau nom est différent de l'ancien
-            this._nom = nom; // On met à jour l'attribut _nom avec le nouveau nom
-        }
+    // Méthode qui renvoie la valeur de potentielDeContact
+    public getPotentielDeContact(): number {
+        return this._potentielDeContact;
     }
 }
-// Main pour les tests
-let p1 = new Personnage(80, "Alice");
-let p2 = new Personnage(); // c. Personne = 0 variable p2
 
-p2.setForce(p1.getForce());// e. p2 prends Force p1
-p1.setForce(Personnage.FORCE_MOYENNE); //i. Moyenne = 50
+// Définition de la classe ChoseBienveillante, qui hérite de la classe Chose
+class ChoseBienveillante extends Chose {
+    // Méthode qui modifie la force du personnage en ajoutant la valeur de potentielDeContact de la chose
+    public entrerEnContact(personnage: Personnage): void {
+        personnage.setForce(personnage.getForce() + this.getPotentielDeContact());
+    }
+}
 
-console.log("La force du premier personnage est : " + p1.getForce());
-console.log("La force du deuxième personnage est : " + p2.getForce());
+// Définition de la classe ChoseHostile, qui hérite de la classe Chose
+class ChoseHostile extends Chose {
+    // Méthode qui modifie la force du personnage en soustrayant la valeur de potentielDeContact de la chose
+    public entrerEnContact(personnage: Personnage): void {
+        personnage.setForce(personnage.getForce() - this.getPotentielDeContact());
+    }
+}
+
+
+// Création de deux instances de la classe Personnage avec des paramètres différents
+let p2 = new Personnage(10, "Alice"); // Personnage avec une force de 10 et un nom "Alice"
+let p1 = new Personnage(Personnage.FORCE_MOYENNE); // Personnage avec une force moyenne (5) et sans nom
+
+// Création de deux instances des classes ChoseBienveillante et ChoseHostile avec des paramètres différents
+const choseBienveillante = new ChoseBienveillante(5); // ChoseBienveillante avec une puissance de 5
+const choseHostile = new ChoseHostile(3); // ChoseHostile avec une puissance de 3
+
+// Appel de la méthode entrerEnContact sur les deux instances de la classe Personnage avec les instances des classes ChoseBienveillante et ChoseHostile en paramètres respectivement
+p2.entrerEnContact(choseBienveillante);
+p1.entrerEnContact(choseHostile);
+
+// Affichage de la force des deux instances de la classe Personnage avec leur nom respectif s'il y en a un, sinon seulement la force
+console.log("La force de " + (p2.getNom() !== null ? p2.getNom() + " " : "") + "est : " + p2.getForce());
+console.log("La force de " + (p1.getNom() !== null ? p2.getNom() + " " : "") + "est : " + p1.getForce());
+
+// Demande de saisie du nom d'un nouveau personnage et création d'une nouvelle instance de la classe Personnage avec la force moyenne et le nom saisi
+rl.question('Quel est votre nom ? ', (nom: string) => {
+    let p3 = new Personnage(Personnage.FORCE_GRANDE, nom);
+    console.log(`La force de ${p3.getNom()} est : ${p3.getForce()}`);
+    rl.close();
+});
--- out/Game.js
+++ out/Game.js
@@ -1,21 +1,37 @@
-// Définition de la classe Personnage
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 var Personnage = /** @class */ (function () {
-    // Définition du constructeur de la classe Personnage, prenant une force initiale et un nom optionnels
     function Personnage(forceInitiale, nom) {
         if (forceInitiale === void 0) { forceInitiale = Personnage.FORCE_ZERO; }
         if (nom === void 0) { nom = ""; }
         this._force = forceInitiale; // Initialisation de l'attribut _force avec la valeur passée en paramètre ou la valeur par défaut FORCE_ZERO
-        this._nom = nom; // Initialisation de l'attribut _nom avec la valeur passée en paramètre ou une chaîne vide
+        this._nom = nom !== null && nom !== "" ? nom : null;
     }
-    // Définition d'un getter pour l'attribut _force
     Personnage.prototype.getForce = function () {
         return this._force;
     };
-    // Définition d'un setter pour l'attribut _force
     Personnage.prototype.setForce = function (force) {
-        if (this._force !== force) { // Si la nouvelle force est différente de l'ancienne
-            this._force = force; // On met à jour l'attribut _force avec la nouvelle force
-        }
+        this._force = force;
     };
     // Définition d'un getter pour l'attribut _nom
     Personnage.prototype.getNom = function () {
@@ -23,8 +39,19 @@ var Personnage = /** @class */ (function () {
     };
     // Définition d'un setter pour l'attribut _nom
     Personnage.prototype.setNom = function (nom) {
-        if (this._nom !== nom) { // Si le nouveau nom est différent de l'ancien
-            this._nom = nom; // On met à jour l'attribut _nom avec le nouveau nom
+        if (nom !== null && nom !== "") {
+            this._nom = nom;
+        }
+    };
+    Personnage.prototype.entrerEnContact = function (chose) {
+        this.modifierForce(chose);
+    };
+    Personnage.prototype.modifierForce = function (chose) {
+        if (chose instanceof ChoseBienveillante) {
+            this._force += chose.getPotentielDeContact();
+        }
+        else if (chose instanceof ChoseHostile) {
+            this._force -= chose.getPotentielDeContact();
         }
     };
     // Définition de constantes statiques pour les forces possibles
@@ -34,11 +61,51 @@ var Personnage = /** @class */ (function () {
     Personnage.FORCE_ZERO = 0;
     return Personnage;
 }());
-// Main pour les tests
-var p1 = new Personnage(80, "Alice");
-var p2 = new Personnage(); // c. Personne = 0 variable p2
-p2.setForce(p1.getForce()); // e. p2 prends Force p1
-p1.setForce(Personnage.FORCE_MOYENNE); //i. Moyenne = 50
-console.log("La force du premier personnage est : " + p1.getForce());
-console.log("La force du deuxième personnage est : " + p2.getForce());
+var Chose = /** @class */ (function () {
+    function Chose(potentielDeContact) {
+        this._potentielDeContact = potentielDeContact;
+    }
+    Chose.prototype.getPotentielDeContact = function () {
+        return this._potentielDeContact;
+    };
+    return Chose;
+}());
+var ChoseBienveillante = /** @class */ (function (_super) {
+    __extends(ChoseBienveillante, _super);
+    function ChoseBienveillante() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ChoseBienveillante.prototype.entrerEnContact = function (personnage) {
+        personnage.setForce(personnage.getForce() + this.getPotentielDeContact());
+    };
+    return ChoseBienveillante;
+}(Chose));
+var ChoseHostile = /** @class */ (function (_super) {
+    __extends(ChoseHostile, _super);
+    function ChoseHostile() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ChoseHostile.prototype.entrerEnContact = function (personnage) {
+        personnage.setForce(personnage.getForce() - this.getPotentielDeContact());
+    };
+    return ChoseHostile;
+}(Chose));
+// Création de deux instances de la classe Personnage avec des paramètres différents
+var p2 = new Personnage(10, "Alice"); // Personnage avec une force de 10 et un nom "Alice"
+var p1 = new Personnage(Personnage.FORCE_MOYENNE); // Personnage avec une force moyenne (5) et sans nom
+// Création de deux instances des classes ChoseBienveillante et ChoseHostile avec des paramètres différents
+var choseBienveillante = new ChoseBienveillante(5); // ChoseBienveillante avec une puissance de 5
+var choseHostile = new ChoseHostile(3); // ChoseHostile avec une puissance de 3
+// Appel de la méthode entrerEnContact sur les deux instances de la classe Personnage avec les instances des classes ChoseBienveillante et ChoseHostile en paramètres respectivement
+p2.entrerEnContact(choseBienveillante);
+p1.entrerEnContact(choseHostile);
+// Affichage de la force des deux instances de la classe Personnage avec leur nom respectif s'il y en a un, sinon seulement la force
+console.log("La force de " + (p2.getNom() !== null ? p2.getNom() + " " : "") + "est : " + p2.getForce());
+console.log("La force de " + (p1.getNom() !== null ? p2.getNom() + " " : "") + "est : " + p1.getForce());
+// Demande de saisie du nom d'un nouveau personnage et création d'une nouvelle instance de la classe Personnage avec la force moyenne et le nom saisi
+rl.question('Quel est votre nom ? ', function (nom) {
+    var p3 = new Personnage(Personnage.FORCE_GRANDE, nom);
+    console.log("La force de ".concat(p3.getNom(), " est : ").concat(p3.getForce()));
+    rl.close();
+});
 //# sourceMappingURL=Game.js.map
