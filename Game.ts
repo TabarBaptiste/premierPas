43be1bfdfb4545091b0b0e18bf436eefbf6fea4a
Baptiste <tabarbaptiste@gmail.com>
2023-03-04 02:28:09 +0100

    first commit

--- Game.ts
+++ Game.ts
@@ -1,28 +1,59 @@
-enum Force {
-    FORCE_ZERO = 0,
-    FORCE_PETITE = 20,
-    FORCE_MOYENNE = 50,
-    FORCE_GRANDE = 80
-}
+// Définition d'un type union pour les forces possibles
+type Force =
+    typeof Personnage.FORCE_ZERO
+    | typeof Personnage.FORCE_PETITE
+    | typeof Personnage.FORCE_MOYENNE
+    | typeof Personnage.FORCE_GRANDE;
 
+// Définition de la classe Personnage
 class Personnage {
-    private _force: Force
 
-    public constructor(forceInitiale: Force = Force.FORCE_ZERO) {
-        this._force = forceInitiale
+    // Définition de constantes statiques pour les forces possibles
+    static readonly FORCE_PETITE = 20;
+    static readonly FORCE_MOYENNE = 50;
+    static readonly FORCE_GRANDE = 80;
+    static readonly FORCE_ZERO = 0;
+
+    // Définition des attributs privés de la classe
+    private _force: Force;
+    private _nom: string;
+
+    // Définition du constructeur de la classe Personnage, prenant une force initiale et un nom optionnels
+    public constructor(forceInitiale: Force = Personnage.FORCE_ZERO, nom: string = "") {
+        this._force = forceInitiale; // Initialisation de l'attribut _force avec la valeur passée en paramètre ou la valeur par défaut FORCE_ZERO
+        this._nom = nom; // Initialisation de l'attribut _nom avec la valeur passée en paramètre ou une chaîne vide
     }
 
+    // Définition d'un getter pour l'attribut _force
     public getForce(): Force {
-        return this._force
+        return this._force;
     }
 
+    // Définition d'un setter pour l'attribut _force
     public setForce(force: Force): void {
-        if (this._force !== force) {
-            this._force = force
+        if (this._force !== force) { // Si la nouvelle force est différente de l'ancienne
+            this._force = force; // On met à jour l'attribut _force avec la nouvelle force
+        }
+    }
+
+    // Définition d'un getter pour l'attribut _nom
+    public getNom(): string {
+        return this._nom;
+    }
+
+    // Définition d'un setter pour l'attribut _nom
+    public setNom(nom: string): void {
+        if (this._nom !== nom) { // Si le nouveau nom est différent de l'ancien
+            this._nom = nom; // On met à jour l'attribut _nom avec le nouveau nom
         }
     }
 }
-
 // Main pour les tests
-let p1 = new Personnage(80)
-console.log("La force du premier personnage est : " + p1.getForce())
+let p1 = new Personnage(80, "Alice");
+let p2 = new Personnage(); // c. Personne = 0 variable p2
+
+p2.setForce(p1.getForce());// e. p2 prends Force p1
+p1.setForce(Personnage.FORCE_MOYENNE); //i. Moyenne = 50
+
+console.log("La force du premier personnage est : " + p1.getForce());
+console.log("La force du deuxième personnage est : " + p2.getForce());
--- out/Game.js
+++ out/Game.js
@@ -1,26 +1,44 @@
-var Force;
-(function (Force) {
-    Force[Force["FORCE_ZERO"] = 0] = "FORCE_ZERO";
-    Force[Force["FORCE_PETITE"] = 20] = "FORCE_PETITE";
-    Force[Force["FORCE_MOYENNE"] = 50] = "FORCE_MOYENNE";
-    Force[Force["FORCE_GRANDE"] = 80] = "FORCE_GRANDE";
-})(Force || (Force = {}));
+// Définition de la classe Personnage
 var Personnage = /** @class */ (function () {
-    function Personnage(forceInitiale) {
-        if (forceInitiale === void 0) { forceInitiale = Force.FORCE_ZERO; }
-        this._force = forceInitiale;
+    // Définition du constructeur de la classe Personnage, prenant une force initiale et un nom optionnels
+    function Personnage(forceInitiale, nom) {
+        if (forceInitiale === void 0) { forceInitiale = Personnage.FORCE_ZERO; }
+        if (nom === void 0) { nom = ""; }
+        this._force = forceInitiale; // Initialisation de l'attribut _force avec la valeur passée en paramètre ou la valeur par défaut FORCE_ZERO
+        this._nom = nom; // Initialisation de l'attribut _nom avec la valeur passée en paramètre ou une chaîne vide
     }
+    // Définition d'un getter pour l'attribut _force
     Personnage.prototype.getForce = function () {
         return this._force;
     };
+    // Définition d'un setter pour l'attribut _force
     Personnage.prototype.setForce = function (force) {
-        if (this._force !== force) {
-            this._force = force;
+        if (this._force !== force) { // Si la nouvelle force est différente de l'ancienne
+            this._force = force; // On met à jour l'attribut _force avec la nouvelle force
         }
     };
+    // Définition d'un getter pour l'attribut _nom
+    Personnage.prototype.getNom = function () {
+        return this._nom;
+    };
+    // Définition d'un setter pour l'attribut _nom
+    Personnage.prototype.setNom = function (nom) {
+        if (this._nom !== nom) { // Si le nouveau nom est différent de l'ancien
+            this._nom = nom; // On met à jour l'attribut _nom avec le nouveau nom
+        }
+    };
+    // Définition de constantes statiques pour les forces possibles
+    Personnage.FORCE_PETITE = 20;
+    Personnage.FORCE_MOYENNE = 50;
+    Personnage.FORCE_GRANDE = 80;
+    Personnage.FORCE_ZERO = 0;
     return Personnage;
 }());
 // Main pour les tests
-var p1 = new Personnage(80);
+var p1 = new Personnage(80, "Alice");
+var p2 = new Personnage();
+p2.setForce(p1.getForce());
+p1.setForce(Personnage.FORCE_MOYENNE);
 console.log("La force du premier personnage est : " + p1.getForce());
+console.log("La force du deuxième personnage est : " + p2.getForce());
 //# sourceMappingURL=Game.js.map
